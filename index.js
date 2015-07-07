89ef1a2053ff8707b69d5017adb666287b8cabe2
Graham Baker <graham.baker@accretivetg.com>
2015-07-07 14:01:06 -0700

    init the project!!

---
+++ index.js
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2015 Ari Aosved
+ * http://github.com/devaos/xrate/blob/master/LICENSE
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  var fs = require('fs')
+    , xrate = this
+
+  this.base = '/sys/class/net'
+  this.device = 'eth0'
+  this.sent = 'statistics/rx_bytes'
+  this.rcvd = 'statistics/tx_bytes'
+}	
+
+		
+
+
---
+++ src/xrate.js
@@ -0,0 +1,20 @@
+/**
+ * @license xrate
+ * Copyright (c) 2014 Accretive Technology Group, Inc.  http://accretivetg.com
+ * For use only by Accretive Technology Group, Inc., its employees and
+ * contractors. All rights reserved.
+ *
+ * @file xrate.js - provides information about incoming and outgoing bandwidth 
+ */
+
+
+var async = require('async')
+	, fs = require('fs')
+	, winston = require('winston')
+
+
+var config = {
+	frequency : 1000, 
+	units : 'bytes', 
+	update: false  
+}
